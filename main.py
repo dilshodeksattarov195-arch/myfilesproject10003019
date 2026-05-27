8530734493a241854a6579e79448633de4759093
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-28 00:49:40 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class userSeleteWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = userSeleteWorker()
+    print("Worker engine initialized.")
